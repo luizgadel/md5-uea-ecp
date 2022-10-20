c336c42db599b6eec84ac8b2d379bb4a8b931f29
Luiz Gadelha <lpgds.eng16@uea.edu.br>
2022-10-20 18:10:48 -0400

    passo 3: inicialização do buffer MD

--- md5.py
+++ md5.py
@@ -1,10 +1,11 @@
 def imprimeBinario(bin):
-    if (len(bin) > 20): 
-        msg = f"Tam: {len(bin)} - Bin: {bin[:10]}...{bin[-10:]}" 
-    else: 
-        msg = f"Tam: {len(bin)} - Bin: {bin}"
-    
+    if (len(bin) > 20):
+        msg = f"{len(bin)} - {bin[:10]}...{bin[-10:]}"
+    else:
+        msg = f"{len(bin)} - {bin}"
+
     print(msg)
+
 
 def converteStrParaBinario(msg):
     bin_msg = ""
@@ -12,20 +13,23 @@ def converteStrParaBinario(msg):
         bin_msg += bin(ord(caractere))[2:]
     return bin_msg
 
+
 def extensao_passo_1(msg_binaria):
     congruent = 448
     modulo = 512
 
     msg_estendida = (msg_binaria + '1')
-    
+
     tam_atual_msg = len(msg_estendida)
     if (tam_atual_msg >= congruent):
-        tam_final = ((tam_atual_msg - congruent) // modulo + 1) * modulo + congruent
-    else: 
+        tam_final = ((tam_atual_msg - congruent) //
+                     modulo + 1) * modulo + congruent
+    else:
         tam_final = congruent
 
     msg_estendida = msg_estendida.ljust(tam_final, '0')
     return msg_estendida
+
 
 def converteIntParaBinario(x, tam_final):
     x_em_binario = bin(x)[2:]
@@ -39,6 +43,27 @@ def extensao_passo_2(msg_passo_1, b):
     b_64bit = converteIntParaBinario(b, 64)
     return msg_passo_1 + b_64bit
 
+def reversed_string(str):
+    return str[::-1]
+
+def initialize_md_buffer():
+    MD_buffer_hex = {
+        "A": "01234567",
+        "B": "89abcdef",
+        "C": "fedcba98",
+        "D": "76543210"
+    }
+    MD_buffer = {}
+    nibble_len = 4
+    for w in MD_buffer_hex:
+        nibble_bin = ""
+        for nibble_hex in MD_buffer_hex[w]:
+            nibble_bin += reversed_string(bin(int(nibble_hex, base=16))[2:].zfill(nibble_len))
+        MD_buffer[w] = nibble_bin
+    
+    return MD_buffer
+    
+
 mensagem = input("Digite a mensagem:")
 tam_msg_original = len(mensagem)
 msg_binaria = converteStrParaBinario(mensagem)
@@ -51,3 +76,7 @@ imprimeBinario(msg_passo_1)
 print("\nPasso 2: Extensão com o valor do tamanho")
 msg_passo_2 = extensao_passo_2(msg_passo_1, tam_msg_original)
 imprimeBinario(msg_passo_2)
+
+print("\nPasso 3: Inicializar Buffer MD")
+MD_buffer = initialize_md_buffer()
+print(MD_buffer)
